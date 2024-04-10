57d4572c50738e792d5577b5d80ff69979efc201
Hey-Calm <69107468+Hey-Calm@users.noreply.github.com>
2024-04-10 09:33:29 +0800

    Add files via upload

---
+++ Source code/Calculate_arctan.py
@@ -0,0 +1,20 @@
+##函数功能##
+#实现arctan函数
+#输入 x ：需要计算的反三函数值   
+#输出 result_rad ：返回弧度制角度    (-pi/2,pi/2)
+#    result_deg: 返回角度 
+
+import math
+def Calculate_arctan(x):
+    n=10
+    result_rad = 0
+    sign = 1
+    power = x
+    for i in range(1,n+1):
+        term = power/(2*i-1)
+        result_rad += sign *term
+        power *= x**2
+        sign *= -1
+
+    result_deg = result_rad*180/math.pi    
+    return result_rad,result_deg
