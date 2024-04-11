09e0de73cd5a7c7b3ce2bfd57b35c82f2d716675
fanduowen <1292065894@qq.com>
2024-04-11 17:33:37 +0800

    update code

--- Source code/functionchoose.py
+++ Source code/functionchoose.py
@@ -17,7 +17,8 @@ def confirm():
            5: 'arctan(反正切)',
            }
     setting1 = dic.get(var.get())
-    answer = tkinter.messagebox.askokcancel('Confirm/Cancel', 'Your choice is '+ setting1 +',''please select Confirm or Cancel.')
+    # answer = tkinter.messagebox.askokcancel('Confirm/Cancel', 'Your choice is '+ setting1 +',''please select Confirm or Cancel.')
+    answer = tkinter.messagebox.askokcancel('确认选择', '你选择的计算函数为'+ setting1 )
     if answer:
         if var.get() == 1 or var.get() == 2 or \
             var.get() == 3 or var.get() == 4 or var.get() == 5:
@@ -38,9 +39,9 @@ lb.pack()
 root.geometry('350x350') #设置窗口大小
 
 
-msg8 = Label(root, text='Confirm',
-                       bg='#ADD8E6', \
-                       font=('微软雅黑', 10))
+# msg8 = Label(root, text='Confirm',
+#                        bg='#ADD8E6', \
+#                        font=('微软雅黑', 10))
 var = IntVar() #选择项
 
 btn1 = Radiobutton(root, text='sin(正弦)', bg="#ADD8E6", activebackground='gray', variable=var,
@@ -63,9 +64,9 @@ btn5 = Radiobutton(root, text='arctan(反正切)', bg="#ADD8E6", activebackgroun
                            value=5)
 btn5.place(relx=0.22, y=240, relheight=0.05, width=200)
 
-msg8.place(relx=0.22, y=280, relheight=0.05, width=200)
+# msg8.place(relx=0.22, y=280, relheight=0.05, width=200)
 btn8 = Button(root, text='确认', activebackground='gray', command=confirm)
-btn8.place(relx=0.22, y=300, relheight=0.05, width=200)
+btn8.place(relx=0.22, y=270, relheight=0.1, width=200)
 #保存选项
 root.mainloop()
 
