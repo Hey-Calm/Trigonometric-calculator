04b40685cb336c1e0ec4e2a0fe840414a78c3e06
Hey-Calm <69107468+Hey-Calm@users.noreply.github.com>
2024-04-11 10:31:54 +0800

    Add files via upload

---
+++ Source code/sin_cos_rad.py
@@ -0,0 +1,34 @@
+
+# -*- coding: utf-8 -*-
+
+##输入参数为弧度制
+
+#计算阶乘
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+def Calculate_sin(x):
+    result = 0;
+    for i in range(10):
+        term = (-1)**i*x**(2*i+1)/factorial(2*i+1)
+        result += term
+    return result
+
+
+def Calculate_cos(x):
+    result = 0;
+    for i in range(10):
+        term = (-1)**i*x**(2*i)/factorial(2*i)
+        result += term
+    return result
+    
+def Calculate_tan(x):
+    sin_result = Calculate_sin(x)
+    cos_result = Calculate_cos(x)
+    return sin_result / cos_result   
+    
+
