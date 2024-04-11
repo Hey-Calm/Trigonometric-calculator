2d9775c7659807462b5e3262520b2540b7252ad5
cyl0514 <165743182+cyl0514@users.noreply.github.com>
2024-04-11 20:59:31 +0800

    Add files via upload

--- Source code/Interaction.py
+++ Source code/Interaction.py
@@ -1,17 +1,28 @@
 from tkinter import *
 import Calculate_arctan
-#import Cal_sin_cos_rad
-#import Cal_sin_cos_degree
+import Cal_sin_cos_rad
+import Cal_sin_cos_degree
+import Arcsin_cal
 import tkinter.messagebox
-import numpy as np
+
 
 '''将用户设置下发至交互模块'''
 def BeginSimulation(set1, set2_1, set2_2, set2_3):
-    global Function, choice1, Angle, Radian
+    global Function, choice1, Angle, Rad
     Function = set1
     choice1 = set2_1
     Angle = set2_2
-    Radian = set2_3
+    Rad = set2_3
+
+def confirm():
+    answer = tkinter.messagebox.askokcancel('确认/取消',
+                                            '请选择是否重新输入计算')
+    if answer:
+        root.destroy()
+        import functionchoose
+        functionchoose.root.mainloop()
+
+
 
 def Show3():
     global root
@@ -35,21 +46,86 @@ def Show3():
            }
     setting1 = dic.get(Function)
 
-    arctan_result = Calculate_arctan.Calculate_arctan(int(Angle))
-    arctan_result_rad = arctan_result[0]
-    arctan_result_deg = arctan_result[1]
-
-
     if choice1 == 1:
-        lb1 = Label(root, text='角度制：' + setting1 + '(' + Angle + ')' + ' = ' + str(result_deg), font=('微软雅黑', 10), bg='LightGrey')
-        lb2 = Label(root, text='结果误差：', font=('微软雅黑', 10), bg='LightGrey')
-        lb1.place(relx=0, y=50, relheight=0.3, width=400)
-        lb2.place(relx=0, y=120, relheight=0.3, width=400)
+        if Function ==1:
+            sin_result_deg = Cal_sin_cos_degree.Calculate_sin(int(Angle))
+            lb1 = Label(root, text='角度制：' + setting1 + '(' + Angle + ')' + ' = ' + str(sin_result_deg),
+                        font=('微软雅黑', 10), bg='LightGrey')
+            lb2 = Label(root, text='结果误差：', font=('微软雅黑', 10), bg='LightGrey')
+            lb1.place(relx=0, y=50, relheight=0.3, width=400)
+            lb2.place(relx=0, y=120, relheight=0.3, width=400)
+        if Function ==2:
+            cos_result_deg = Cal_sin_cos_degree.Calculate_cos(int(Angle))
+            lb1 = Label(root, text='角度制：' + setting1 + '(' + Angle + ')' + ' = ' + str(cos_result_deg),
+                        font=('微软雅黑', 10), bg='LightGrey')
+            lb2 = Label(root, text='结果误差：', font=('微软雅黑', 10), bg='LightGrey')
+            lb1.place(relx=0, y=50, relheight=0.3, width=400)
+            lb2.place(relx=0, y=120, relheight=0.3, width=400)
+        if Function ==3:
+            tan_result_deg = Cal_sin_cos_degree.Calculate_tan(int(Angle))
+            lb1 = Label(root, text='角度制：' + setting1 + '(' + Angle + ')' + ' = ' + str(tan_result_deg),
+                        font=('微软雅黑', 10), bg='LightGrey')
+            lb2 = Label(root, text='结果误差：', font=('微软雅黑', 10), bg='LightGrey')
+            lb1.place(relx=0, y=50, relheight=0.3, width=400)
+            lb2.place(relx=0, y=120, relheight=0.3, width=400)
+        if Function ==4:
+            arcsin_result = Arcsin_cal.Arcsin_calc(int(Angle))
+            arcsin_result_deg = arcsin_result[1]
+            lb1 = Label(root, text='角度制：' + setting1 + '(' + Angle + ')' + ' = ' + str(arcsin_result_deg),
+                        font=('微软雅黑', 10), bg='LightGrey')
+            lb2 = Label(root, text='结果误差：', font=('微软雅黑', 10), bg='LightGrey')
+            lb1.place(relx=0, y=50, relheight=0.3, width=400)
+            lb2.place(relx=0, y=120, relheight=0.3, width=400)
+        if Function ==5:
+            arctan_result = Calculate_arctan.Calculate_arctan(int(Angle))
+            arctan_result_deg = arctan_result[1]
+            lb1 = Label(root, text='角度制：' + setting1 + '(' + Angle + ')' + ' = ' + str(arctan_result_deg),
+                        font=('微软雅黑', 10), bg='LightGrey')
+            lb2 = Label(root, text='结果误差：', font=('微软雅黑', 10), bg='LightGrey')
+            lb1.place(relx=0, y=50, relheight=0.3, width=400)
+            lb2.place(relx=0, y=120, relheight=0.3, width=400)
     if choice1 == 2:
-        lb1 = Label(root, text='弧度制：' + setting1 + '(' + Radian+ ')' + ' = ' + str(arctan_rad), font=('微软雅黑', 10), bg='LightGrey')
-        lb2 = Label(root, text='结果误差：', font=('微软雅黑', 10), bg='LightGrey')
-        lb1.place(relx=0, y=50, relheight=0.3, width=400)
-        lb2.place(relx=0, y=120, relheight=0.3, width=400)
+        if Function ==1:
+            sin_result_rad = Cal_sin_cos_rad.Calculate_sin(int(Rad))
+            lb1 = Label(root, text='弧度制：' + setting1 + '(' + Rad + ')' + ' = ' + str(sin_result_rad),
+                        font=('微软雅黑', 10), bg='LightGrey')
+            lb2 = Label(root, text='结果误差：', font=('微软雅黑', 10), bg='LightGrey')
+            lb1.place(relx=0, y=50, relheight=0.3, width=400)
+            lb2.place(relx=0, y=120, relheight=0.3, width=400)
+        if Function ==2:
+            cos_result_Rad = Cal_sin_cos_rad.Calculate_cos(int(Rad))
+            lb1 = Label(root, text='弧度制：' + setting1 + '(' + Rad + ')' + ' = ' + str(cos_result_Rad),
+                        font=('微软雅黑', 10), bg='LightGrey')
+            lb2 = Label(root, text='结果误差：', font=('微软雅黑', 10), bg='LightGrey')
+            lb1.place(relx=0, y=50, relheight=0.3, width=400)
+            lb2.place(relx=0, y=120, relheight=0.3, width=400)
+        if Function ==3:
+            tan_result_Rad = Cal_sin_cos_rad.Calculate_tan(int(Rad))
+            lb1 = Label(root, text='弧度制：' + setting1 + '(' + Rad + ')' + ' = ' + str(tan_result_Rad),
+                        font=('微软雅黑', 10), bg='LightGrey')
+            lb2 = Label(root, text='结果误差：', font=('微软雅黑', 10), bg='LightGrey')
+            lb1.place(relx=0, y=50, relheight=0.3, width=400)
+            lb2.place(relx=0, y=120, relheight=0.3, width=400)
+        if Function ==4:
+            arcsin_result = Arcsin_cal.Arcsin_calc(int(Rad))
+            arcsin_result_Rad = arcsin_result[0]
+            lb1 = Label(root, text='弧度制：' + setting1 + '(' + Rad + ')' + ' = ' + str(arcsin_result_Rad),
+                        font=('微软雅黑', 10), bg='LightGrey')
+            lb2 = Label(root, text='结果误差：', font=('微软雅黑', 10), bg='LightGrey')
+            lb1.place(relx=0, y=50, relheight=0.3, width=400)
+            lb2.place(relx=0, y=120, relheight=0.3, width=400)
+        if Function ==5:
+            arctan_result = Calculate_arctan.Calculate_arctan(int(Rad))
+            arctan_result_Rad = arctan_result[0]
+            lb1 = Label(root, text='角度制：' + setting1 + '(' + Rad + ')' + ' = ' + str(arctan_result_Rad),
+                        font=('微软雅黑', 10), bg='LightGrey')
+            lb2 = Label(root, text='结果误差：', font=('微软雅黑', 10), bg='LightGrey')
+            lb1.place(relx=0, y=50, relheight=0.3, width=400)
+            lb2.place(relx=0, y=120, relheight=0.3, width=400)
+
+    '''设置回退任务按钮'''
+    performbtn = Button(root, text='确认', activebackground='gray',command = confirm)
+    performbtn.place(relx=0.2, y=260, relheight=0.1, width=200)
     root.mainloop()
 
 #def resultshow1():
