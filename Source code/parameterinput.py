02b53cad12a7e6814b2c788e44bb4659a044a6dc
cyl0514 <165743182+cyl0514@users.noreply.github.com>
2024-04-12 20:11:54 +0800

    Update parameterinput.py

--- Source code/parameterinput.py
+++ Source code/parameterinput.py
@@ -1,18 +1,26 @@
 from tkinter import *
 import tkinter.messagebox
-from tkinter.simpledialog import *
 import Interaction
+import Interaction_arc
 
 '''第二个界面的数据信息'''
 def valueget():
     Interaction.BeginSimulation(setting1, var1_1.get(), angle.get(), radian.get())
+
+def valueget_arc():
+    Interaction_arc.BeginSimulation_arc(setting1, inputnum.get())
 def confirmcalculate():
-    answer = tkinter.messagebox.askokcancel('Confirm/Cancel',
-                                            'Data has saved successfully, please select Confirm or Cancel.')
+    answer = tkinter.messagebox.askokcancel('确认/取消',
+                                            '数据已经成功保存，请选择确认或者取消！')
     if answer:
-        valueget()
-        root.destroy()
-        Interaction.Show3()
+        if setting1 == 1 or setting1 == 2 or setting1 == 3:
+            valueget()
+            root.destroy()
+            Interaction.Show3()
+        if setting1 == 4 or setting1 == 5:
+            valueget_arc()
+            root.destroy()
+            Interaction_arc.Show4()
 def choicejudge():
     global angle
     global number
@@ -29,12 +37,13 @@ def choicejudge():
         radian = Entry(width=50)
         radian.place(relx=0.22, y=180, relheight=0.05, width=200)
 
-def Show2(window,set1):
+def Show2(window2,set1):
     global root
     global setting1
     global var1_1
+    global inputnum
     setting1 = set1
-    root = window
+    root = window2
     root.title('输入')#标题
     root.geometry('350x350')  # 设置窗口大小
     var1_1 = IntVar()
@@ -42,13 +51,20 @@ def Show2(window,set1):
     lb1 = Label(root, text='参数输入', font=('Times', 15), bg='#ADD8E6',relief=SUNKEN,anchor='center')
     lb1.place(relx=0, y=0, relwidth=1, relheight=0.1)
 
-    btn1 = Radiobutton(root, text='角度', bg="#ADD8E6", activebackground='gray', variable=var1_1,
-                       value=1, command = choicejudge)
-    btn1.place(relx=0.22, y=80, relheight=0.05, width=200)
+    if setting1 == 1 or  setting1 == 2 or setting1 == 3:
+        btn1 = Radiobutton(root, text='角度', bg="#ADD8E6", activebackground='gray', variable=var1_1,
+                             value=1, command = choicejudge)
+        btn1.place(relx=0.22, y=80, relheight=0.05, width=200)
 
-    btn2 = Radiobutton(root, text='弧度', bg="#ADD8E6", activebackground='gray', variable=var1_1,
-                       value=2, command = choicejudge)
-    btn2.place(relx=0.22, y=160, relheight=0.05, width=200)
+        btn2 = Radiobutton(root, text='弧度', bg="#ADD8E6", activebackground='gray', variable=var1_1,
+                            value=2, command = choicejudge)
+        btn2.place(relx=0.22, y=160, relheight=0.05, width=200)
+
+    if setting1 == 4 or setting1 == 5:
+        msg = Label(root, text='计算值',bg='#ADD8E6', font=('微软雅黑', 10))
+        msg.place(relx=0.22, y=120, relheight=0.05, width=200)
+        inputnum = Entry(width=50)
+        inputnum.place(relx=0.22, y=140, relheight=0.05, width=200)
 
     '''设置计算任务按钮'''
     perform = Label(root, text='Calculate', font=('微软雅黑', 10), bg='#ADD8E6')
